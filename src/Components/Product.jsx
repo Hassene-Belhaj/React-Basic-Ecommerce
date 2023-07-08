647403a63ee24cc19312ace7345c2b9786942534
Hassene-Belhaj <hassene.belhaj@gmail.com>
2023-07-08 22:06:32 +0200

    add category all  & restyling

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -116,7 +116,7 @@ const Product = ({product}) => {
             <ProductDiv onMouseEnter={mouseEnter} onMouseLeave={mouseLeaving}>
                 <img src={image} alt="" />
 
-                <ProductsItems style={{width: hovering ? "100%" : 0 }} >
+                <ProductsItems style={{width: hovering ? 0 : '-100%'}} >
                     {hovering ?   
                     <>
                     <motion.h3 style={{textTransform:'capitalize'}}
